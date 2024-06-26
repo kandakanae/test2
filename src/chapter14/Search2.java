713d1dd910245c0519e1faca6bcdbe76ae995fbe
ngn_teacher_0005 <ngn_teacher_0005@ngn_teacher_005>
2024-06-26 09:01:12 +0900

    旧PC試し

--- src/chapter14/Search2.java
+++ src/chapter14/Search2.java
@@ -38,10 +38,7 @@ public class Search2 extends HttpServlet {
 			ResultSet rs=st.executeQuery();
 
 			while (rs.next()) {
-				out.println(rs.getInt("id"));
-				out.println(rs.getInt("id"));
 				out.println("：");
-				out.println(rs.getString("name"));
 				out.println("：");
 				out.println(rs.getInt("price"));
 
